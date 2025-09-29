b99a3cc035391176e7a46d929a3bfbdcf7bf1bc7
shabrin-begum-15 <shabrinbegum1510@gmail.com>
2025-09-29 16:51:19 +0530

    Build dist folder and initial project files

--- Server.js
+++ Server.js
@@ -22,6 +22,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
